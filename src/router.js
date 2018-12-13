a07927fc3413873831b13a2f254aeda03d648f57
Martin Ahindura <martinahindura@gmail.com>
2018-12-13 11:25:39 +0300

    Revert back to lazy loaded route components

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import AppList from "./views/AppList.vue";
-import AppDetail from "./views/AppDetail.vue";
+// import AppList from "./views/AppList.vue";
 
 Vue.use(Router);
 
@@ -12,23 +11,19 @@ export default new Router({
     {
       path: "/",
       name: "app-list",
-      component: AppList
-      // Lazy-load the view
-      // component: () => import("./views/AppList.vue")
-      // Lazy loading seems to be causing issues with github pages
+      // component: AppList, // Lazy-load the view
+      component: () => import("./views/AppList.vue")
     },
     {
       path: "/:id",
       name: "app-detail",
-      component: AppDetail
-      // component: () => import("./views/AppDetail.vue")
+      component: () => import("./views/AppDetail.vue")
     },
     {
       path: "*", // for  any random url
       name: "app-all",
-      component: AppList
-      // Lazy-load the view
-      // component: () => import("./views/AppList.vue")
+      // component: AppList, // Lazy-load the view
+      component: () => import("./views/AppList.vue")
     }
   ]
 });
